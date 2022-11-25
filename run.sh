05f0b97448e214f6365f9365b850ef589125245a
Stephen Tong <14918218+stong@users.noreply.github.com>
2022-11-25 21:43:33 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 IMAGE="$1"
 PORT="$2"
 HTTP_PORT="$3"
+SHARED_SECRET="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
 
 echo "[+] running challenge"
 exec docker run \
@@ -11,6 +12,7 @@ exec docker run \
     -e "ETH_RPC_URL=$ETH_RPC_URL" \
     -e "FLAG=$FLAG" \
     -e "PUBLIC_IP=$PUBLIC_IP" \
+    -e "SHARED_SECRET=$SHARED_SECRET" \
     -p "$PORT:$PORT" \
     -p "$HTTP_PORT:$HTTP_PORT" \
     "$IMAGE"
